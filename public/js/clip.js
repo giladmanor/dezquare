9e48aaf5baf7e14a249e0843ecfa648e704a17af
lio <leofle@gmail.com>
2012-11-12 16:31:58 +0200

    copy to clip button fix

---
+++ public/js/clip.js
@@ -0,0 +1,29 @@
+	$(document).ready(function(){
+	$('a#copy-description').zclip({
+        path:'http://dezquare.com/js/ZeroClipboard.swf',
+        copy:$('p#description').text()
+    });
+    // The link with ID "copy-description" will copy
+    // the text of the paragraph with ID "description"
+    $('a#copy-dynamic').zclip({
+        path:'http://dezquare.com/js/ZeroClipboard.swf',
+        copy:function(){return $('input#dynamic').val();}
+    });
+    // The link with ID "copy-dynamic" will copy the current value
+    // of a dynamically changing input with the ID "dynamic"
+
+	
+	$("a#copy-callbacks").zclip({
+	        path:'http://dezquare.com/js/ZeroClipboard.swf',
+	        copy:$('#callback-paragraph').text(),
+	        beforeCopy:function(){
+	            $('#callback-paragraph').css('background','yellow');
+	            $(this).css('color','orange');
+	        },
+	        afterCopy:function(){
+	            $('#callback-paragraph').css('background','green');
+	            $(this).css('color','purple');
+	            $(this).next('.check').show();
+	        }
+	    });
+	});
