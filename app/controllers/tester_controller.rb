72564f810093716962e2c519b0366338e04ba10f
Gilad Manor <gilad@quizic.co>
2012-12-19 15:54:05 +0200

    reports

--- app/controllers/tester_controller.rb
+++ app/controllers/tester_controller.rb
@@ -12,9 +12,15 @@ class TesterController < ApplicationController
     
   end
   
-  def clear
-    
-    render :text =>Rails.cache.clear 
+  def itg
+    res = Game.all.select{|g| g.images.size>0}.map{|g| g.images}.flatten.length/Game.all.select{|g| g.images.size>0}.size
+    render :text =>res
+  end
+  
+  def locations
+    res = User.all.select{|d| d.designer && !d.location.nil? && d.location.length>0}.map{|d| d.location}.compact.sort
+    u = res.uniq
+    render :text=> u.map{|i| "#{i} (#{res.count(i)})"}.join("</br>")
   end
   
   
